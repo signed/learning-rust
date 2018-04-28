7e4885a152c4205f8a4652866ca326025f44d62e
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2018-04-28 19:40:06 +0200

    backup local changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+extern crate pulldown_cmark;
+
+use pulldown_cmark::{Parser, Event, Tag};
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
+
+    let markdown_string = "\
+[I'm an inline-style link](https://www.google.com) \
+[I'm a relative reference to a repository file](../blob/master/LICENSE)\
+[You can use numbers for reference-style link definitions][1]
+
+[1]: http://slashdot.org
+";
+
+    let parser = Parser::new(&markdown_string);
+
+    let parser = parser.map(|event| match event {
+        Event::HardBreak => Event::SoftBreak,
+        Event::Start(Tag::Link(one, two)) => event,
+        _ => event
+    });
+
 }
